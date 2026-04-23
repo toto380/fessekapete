6100e549c321c305d96fffb531099bd69a699bc7
toto380 <antoniobft.pro@gmail.com>
2026-04-23 22:47:21 +0200

    Update wv-collect.ts

--- fessekapete/netlify/edge-functions/wv-collect.ts
+++ fessekapete/netlify/edge-functions/wv-collect.ts
@@ -1,7 +1,7 @@
 import { Context } from "https://edge.netlify.com";
 
 export default async function (request: Request, context: Context) {
-  const SGTM_URL = Deno.env.get("WV_SGTM_URL")?.replace(/\/$/, "") || "https://metrics.stratads.fr";
+  const SGTM_URL = Deno.env.get("WV_GTM_SERVER_URL")?.replace(/\/$/, "") || "https://metrics.stratads.fr";
   const incoming = new URL(request.url);
   const target = `${SGTM_URL}/g/collect${incoming.search}`;
 
@@ -13,9 +13,6 @@ export default async function (request: Request, context: Context) {
   }
   headers.delete("host");
   headers.set("X-Gtm-Server-Preview", "ZW52LTV8N0txdUxsMUE2TUFmT3RlRlhpQkJsQXwxOWRiYjllNGM3MjYxNTQ4OTEwODg=");
-
-  const clientIP = (context as any).ip || request.headers.get("x-nf-client-connection-ip") || "";
-  if (clientIP) headers.set("X-Forwarded-For", clientIP);
 
   try {
     const upstream = await fetch(target, {
